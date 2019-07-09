50bc3421459ca143f1e0fdce5158755796362975
adijag <grango153@gmail.com>
2019-07-10 00:04:38 +0530

    first

--- src/main/java/com/vtkty/vtktyacc/controller/InvoiceController.java
+++ src/main/java/com/vtkty/vtktyacc/controller/InvoiceController.java
@@ -2,11 +2,24 @@ package com.vtkty.vtktyacc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/home")
 public class InvoiceController {
-    @GetMapping(value="/")
+
+    @GetMapping(value="/newinvoice" )
     public String newinvoice(){
         return "newinvoice";
+    }
+
+    @GetMapping(value="/options")
+    public String taskoptions(){
+        return "options";
+    }
+
+    @RequestMapping(value = "/redirect")
+    public String redirect() {
+        return "redirect:newinvoice";
     }
 }
