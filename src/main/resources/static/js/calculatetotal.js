8808651e4f001a3e68b9472d5fb695aed7c32d6d
adijag <grango153@gmail.com>
2019-07-19 11:41:43 +0530

    17/07/019

--- src/main/resources/static/js/calculatetotal.js
+++ src/main/resources/static/js/calculatetotal.js
@@ -1,12 +1,40 @@
- function calculateTotal() {
-        var rate = document.getElementById("ticketPrice").value;
-        var noofPax = document.getElementById("noOfTickets").value;
-        var discount = document.getElementById("ticketDiscount").value;
-        var totalInput = document.getElementById("totalPriceJS");
+$(document).ready(function() {
+    //this calculates values automatically
+    calculation();
+    $("#inputNoofPax, #inputRate").on("keydown keyup", function() {
+        calculation();
+    });
+});
 
-        //do all the calculations here
-        var total = price * quantity
-        if (discount) total -= discount;
+function calculation() {
+            var noofPax = document.getElementById('inputNoofPax').value;
+            var rateperPax = document.getElementById('inputRate').value;
+            var amount = document.getElementById('inputAmount').value;
+            var gst = document.getElementById('inputGst').value;
+            var subtotal = document.getElementById('inputSubTotal').value;
+            var nepalRemit = document.getElementById('inputNepalRemitCharges').value;
+            var grandTotal = document.getElementById('inputGrandTotal');
 
-        totalInput.innerHTML = total
-    }
+			var amount = parseInt(noofPax) * parseInt(rateperPax);
+			var gst = 0.05 * amount;
+			var subtotal = amount + gst;
+
+			var fiftyk = 50000;
+
+			if(subtotal<fiftyk)
+			var nepalRemit = 150;
+			else
+			var nepalRemit = 200;
+
+			var grandTotal = subtotal + nepalRemit;
+
+
+            if (!isNaN(grandTotal)) {
+                document.getElementById('inputAmount').value = amount;
+                document.getElementById('inputGst').value = gst;
+                document.getElementById('inputSubTotal').value = subtotal;
+                document.getElementById('inputNepalRemitCharges').value = nepalRemit;
+                document.getElementById('inputGrandTotal').value = grandTotal;
+
+            }
+        }
